1efa9b386425073a5b766d26e90358eb220c85f5
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-01-28 11:42:06 +0530

    Create Program.java

---
+++ JAVA/Program.java
@@ -0,0 +1,62 @@
+
+/*    
+https://www.sololearn.com/Profile/928735/?ref=app   
+Asif Bilakhiya     
+*/ 
+ 
+public class Program   
+{ 
+    public static void main(String[] args)
+    {
+        System.out.println("\n\nDigit[0-9]");
+        for(int i=48;i<=57;i++)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        } 
+        
+        System.out.println("\n\nAlphabet[a-z]");
+        for(int i=97;i<=122;i++)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        } 
+        
+        System.out.println("\n\nAlphabet[A-Z]");
+        for(int i=65;i<=90;i++)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+        System.out.println("\n\nSpecial Char:");
+        
+        for(int i=32;i<=254;i++)
+        {
+        if(i>=32&&i<=47)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+        if(i>=58&&i<=64)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+        if(i>=91&&i<=96)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+        if(i>=123&&i<=126)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+        if(i>=224&&i<=254)
+        {
+            System.out.printf("\n%c=%d",i,i);
+        }
+       // if(91 96) 123 126 224 254
+        }
+           System.out.printf("\n__________________________________________");
+System.out.printf("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.printf("------------------------------------------");
+    
+
+    //System.out.println("\t----------------------------------");
+    
+    }
+}
