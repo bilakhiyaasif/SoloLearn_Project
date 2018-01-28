3bf2a1dcffbffa0a0c3a26d1714e7d3215fbe7f2
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-01-28 11:41:11 +0530

    Create Program.java

---
+++ JAVA/C-6 Number of Digit/Program.java
@@ -0,0 +1,23 @@
+
+import java.util.*; 
+public class Program
+{
+    public static void main(String[] args) 
+ 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();//your num .....
+        int z=n; 
+        int c_1=0;
+        while(z!=0) 
+        {
+            c_1++;
+            z=z/10;  
+        }
+        System.out.println("Num is:"+n);
+        System.out.println("Number of Digit:"+c_1); 
+        System.out.printf("\n__________________________________________");
+System.out.printf("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.printf("------------------------------------------");
+    }
+}
