0daaf6157b6faeab9446dc897fd60e2d00259448
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-01-28 11:58:05 +0530

    Create Program.cpp

---
+++ C_PLUS_PLUS/C-14 goto with label/Program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    a=10;
+    start:a++;           //jump here on start
+    if(a<=20)
+    {
+        cout<<a<<endl;
+        goto start;     //jump to start
+    }
+    
+    // 
+       
+            cout<<"\n__________________________________________";
+cout<<"\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n";
+cout<<"------------------------------------------";
+    
+    return 0;
+}
+
