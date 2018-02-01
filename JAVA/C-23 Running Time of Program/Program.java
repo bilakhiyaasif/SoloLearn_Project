e6eb0d7472dae43ce4b4274c384773664a28fba0
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-02 05:12:45 +0530

    Create Program.java

---
+++ JAVA/C-23 Running Time of Program/Program.java
@@ -0,0 +1,60 @@
+
+//check your Program runtime in Ms
+//thank you for check this
+
+public class Program
+{
+    public static void main(String[] args) {
+        
+         
+         
+        long start=System.currentTimeMillis();
+        
+        //your code
+        //any code to check running time 
+        //thanks for check my code
+        long t=0;
+        boolean t1=true;
+        int j=0;
+        while(t1)
+        {
+            if(t<=5000000)
+            {
+               t++;
+               for(int i=1000;i>=0;i--)
+               {
+               for(int k=1000;k>=0;k--)
+               {  
+               j++;  
+               }
+               }
+                j=0;
+            }
+            if(t==50)
+            {
+                t1=false;
+            }
+        }
+        //
+        //
+        //your code
+        
+        
+        long end=System.currentTimeMillis();
+        long run=end-start;
+        System.out.print("\nRunning Time of your Program is: ");
+         System.out.println(run+" MiliSeconds");
+         
+         
+         
+         
+         
+         
+         
+         System.out.printf("\n__________________________________________");
+System.out.printf("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.printf("------------------------------------------");//
+         
+        
+    }
+}
