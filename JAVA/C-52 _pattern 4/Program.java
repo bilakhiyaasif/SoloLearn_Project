c81ce9360eae583cc9af44738e262c9545f44706
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-18 00:41:17 +0530

    Create Program.java

---
+++ JAVA/C-52 _pattern 4/Program.java
@@ -0,0 +1,40 @@
+/*
+0
+01
+010
+0101
+01010
+
+:Asif Bilakhiya
+*/
+
+
+import java.util.Scanner;
+class Program
+{
+public static void main(String[] args) {
+  int n=(new Scanner(System.in)).nextInt();
+  for(int i=0;i<=n;i++)
+  {
+    for(int j=0;j<i;j++)
+    {
+      if(j%2==0) 
+            {
+            System.out.print("0");
+            }
+    else
+    {
+      System.out.print("1");
+    }
+    }
+    System.out.println("");
+  }
+  developer_asif();
+}
+public static void developer_asif()
+    {
+     System.out.printf("__________________________________________");
+System.out.printf("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.printf("------------------------------------------");//
+    }
+}
