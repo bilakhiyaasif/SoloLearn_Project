cfaae46bc20514566b193b287406f178eba73fb5
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-17 23:41:52 +0530

    Create Solution.java

---
+++ Web/Solution.java
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+     
+    <div id="body-level">
+    
+    <div id="status">
+         Thank you to be here!! 
+        
+    </div>
+    
+    <div id="outer1">
+    <div class="inner1">
+      Challege:1  <a href="https://www.sololearn.com/Discuss/1016741/?ref=app">Click here</a>
+      <span class="left1">20/01/2018</span>
+     </div>
+     
+     
+    <div class="inner1">
+      Challege:2  <a href="https://www.sololearn.com/Discuss/1057964/?ref=app">Click here</a>
+      <span class="left1">07/02/2018</span>
+      
+     </div>
+     
+    
+    
+    
+     <div class="inner1">
+      Challege:3  <a href="https://www.sololearn.com/Discuss/1076046/?ref=app">Click here</a>
+      <span class="left1">16/02/2018</span>
+     </div>
+     
+     <div class="inner1">
+      Challege:4  <a href="#">Comming Soon</a>
+      <span class="left1">__/__/____</span>
+     </div>
+     
+     
+     </div>
+     <div>
+  
+         
+     </div>
+     
+     
+         <div id="div12">
+         Challenges uploaded by <span>&copy;</span>Asif Bilakhiya<span>&trade;</span>
+     </div>
+    <progress id="id1" value="3" min="10" max="10"></progress>
+     </div>
+    </body>
+</html>
