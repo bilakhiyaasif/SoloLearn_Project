f1a6348889bf8b024effba4549490d57b66d388e
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-18 00:32:11 +0530

    Create Program.java

---
+++ JAVA/C-40 pattern_i like it_updated/Program.java
@@ -0,0 +1,63 @@
+
+
+/*
+input:
+any number
+
+      #
+     ##
+    ###
+   ####
+  #####
+ ######
+ 
+*/ 
+import java.util.Scanner;
+public class Program {
+    public static void fnc()
+{  
+              System.out.print("\n__________________________________________");
+System.out.print("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.print("------------------------------------------");
+    }
+    
+    static void ok(int n) {
+     
+        int t=(n-1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j>=t)
+                {
+                    System.out.print("#");
+                    
+                }
+                
+                
+                else
+                {
+                    System.out.print(" ");
+                }
+                
+                
+                
+            }
+            t--;
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ok(n);
+        fnc();
+        in.close();
+    }
+    
+
+    
+    
+    
+}
