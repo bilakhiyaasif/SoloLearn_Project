0f68a4d541038a70ec6b89ac284d73539af0bcd4
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-18 00:40:02 +0530

    Create Program.java

---
+++ JAVA/C-49 _pattern -1/Program.java
@@ -0,0 +1,41 @@
+/*
+1
+22
+333
+4444
+55555
+
+:Asif Bilakhiya
+*/
+
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+        
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=0;
+        for(int i=1;i<=n;i++) 
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+        developer_asif();
+    }
+    
+    
+    
+    
+    
+    public static void developer_asif()
+    {
+     System.out.printf("__________________________________________");
+System.out.printf("\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\t\t\t\t\n|\t| ->Developer:Asif Bilakhiya\t\t\t\t\t\t\t\t\t|\n|\t| ->https://www.sololearn.com/Profile/928735/?ref=app\t\t\t\t\t|\n|\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| if(u_satisfy(true))\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| {\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| like(please);\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n|\t| }\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    System.out.printf("------------------------------------------");//
+   }   
+}
